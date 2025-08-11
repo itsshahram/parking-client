f91ecbf7ce90cc6505bbd6325cbceba7049a355e
Mehdi <mahdimrm01@gmail.com>
2025-08-11 19:58:40 +0330

    add Application_MainPage_ReloadShortcut in settings

--- Parking.App/Settings.Designer.cs
+++ Parking.App/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Parking.App {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -644,6 +644,18 @@ namespace Parking.App {
             }
             set {
                 this["Application_EnableSyncImage"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Application_MainPage_ReloadShortcut {
+            get {
+                return ((string)(this["Application_MainPage_ReloadShortcut"]));
+            }
+            set {
+                this["Application_MainPage_ReloadShortcut"] = value;
             }
         }
     }
