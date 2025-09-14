5c9617b073c68a7b280f75adc3c0580f4585a7aa
Mehdi <mahdimrm01@gmail.com>
2025-09-14 18:29:41 +0330

    Update ExcelHelper.cs

--- Parking.App/Helpers/ExcelHelper.cs
+++ Parking.App/Helpers/ExcelHelper.cs
@@ -1,55 +1,78 @@
 ﻿using ClosedXML.Excel;
 using System.ComponentModel;
 
-namespace Parking.App.Helpers
+namespace Parking.App.Helpers;
+
+public static class ExcelHelper
 {
-    public static class ExcelHelper
+    public static byte[] ExportToExcel<T>(List<T> data, string sheetName = "Sheet1", bool? isRtl = false, string filterInfo = "")
     {
-        public static byte[] ExportToExcel<T>(List<T> data, string sheetName = "Sheet1", bool? isRtl = false)
+        using (var workbook = new XLWorkbook())
         {
-            using (var workbook = new XLWorkbook())
+            workbook.RightToLeft = isRtl ?? false;
+            var worksheet = workbook.Worksheets.Add(sheetName);
+
+            int currentRow = 1;
+            if (!string.IsNullOrWhiteSpace(filterInfo))
             {
-                workbook.RightToLeft = isRtl ?? false;
-                var worksheet = workbook.Worksheets.Add(sheetName);
+                string filterText = $"🔍 فیلتر گزارش: {filterInfo}";
 
-                var properties = typeof(T).GetProperties();
+                var filterRange = worksheet.Range(currentRow, 1, currentRow + 1, 5);
+                filterRange.Merge();
 
-                int col = 1;
+                filterRange.Value = filterText;
+
+                filterRange.Style
+                    .Font.SetBold()
+                    .Fill.SetBackgroundColor(XLColor.LightYellow)
+                    .Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center)   
+                    .Alignment.SetVertical(XLAlignmentVerticalValues.Center)       
+                    .Alignment.SetWrapText(false);
+
+                worksheet.Row(currentRow).AdjustToContents();
+                worksheet.Row(currentRow + 1).Height = worksheet.Row(currentRow).Height; 
+
+                currentRow += 3; 
+            }
+
+            var properties = typeof(T).GetProperties();
+
+            int col = 1;
+            foreach (var prop in properties)
+            {
+                var displayName = prop.GetCustomAttribute<DisplayNameAttribute>()?.DisplayName ?? prop.Name;
+                worksheet.Cell(currentRow, col).Value = displayName;
+                col++;
+            }
+
+            currentRow++;
+
+            foreach (var item in data)
+            {
+                col = 1;
                 foreach (var prop in properties)
                 {
-                    var displayName = prop.GetCustomAttribute<DisplayNameAttribute>()?.DisplayName ?? prop.Name;
-                    worksheet.Cell(1, col).Value = displayName;
+                    object value = prop.GetValue(item);
+
+                    worksheet.Cell(currentRow, col).Value = value switch
+                    {
+                        string str => str,
+                        int num => num,
+                        double dbl => dbl,
+                        bool boolean => boolean,
+                        DateTime dt => dt.ToString("yyyy-MM-dd HH:mm"),
+                        _ => value?.ToString() ?? string.Empty
+                    };
+
                     col++;
                 }
-                int row = 2;
-                foreach (var item in data)
-                {
-                    col = 1;
-                    foreach (var prop in properties)
-                    {
-                        object value = prop.GetValue(item);
+                currentRow++;
+            }
 
-                        // تبدیل مقدار به نوع مناسب برای XLCellValue
-                        worksheet.Cell(row, col).Value = value switch
-                        {
-                            string str => str,
-                            int num => num,
-                            double dbl => dbl,
-                            bool boolean => boolean,
-                            DateTime dt => dt.ToString("yyyy-MM-dd"),
-                            _ => value?.ToString() ?? string.Empty
-                        };
-
-                        col++;
-                    }
-                    row++;
-                }
-
-                using (var stream = new MemoryStream())
-                {
-                    workbook.SaveAs(stream);
-                    return stream.ToArray();
-                }
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                return stream.ToArray();
             }
         }
     }
