979df8ace9757efa704e8f7b1e4d296d8ba47ad5
Mehdi <mahdimrm01@gmail.com>
2025-09-28 12:42:03 +0330

    fix the problem  of scrolling  in main page

--- Parking.App/Views/Components/TicketListView.xaml.cs
+++ Parking.App/Views/Components/TicketListView.xaml.cs
@@ -1,25 +1,13 @@
-﻿using Parking.App.Models.Tickets;
-using Parking.App.Views.Windows;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Forms;
-
-
-namespace Parking.App.Views.Components;
-
+﻿namespace Parking.App.Views.Components;
 
 public partial class TicketListView : UserControl
 {
     public static readonly DependencyProperty ItemsSourceProperty =
         DependencyProperty.Register(
-            "ItemsSource", 
-            typeof(ObservableCollection<TicketsListViewModel>), 
-            typeof(TicketListView), 
-            new PropertyMetadata(null) 
+            "ItemsSource",
+            typeof(ObservableCollection<TicketsListViewModel>),
+            typeof(TicketListView),
+            new PropertyMetadata(null)
         );
 
     public ObservableCollection<TicketsListViewModel> ItemsSource
@@ -33,42 +21,35 @@ public partial class TicketListView : UserControl
         InitializeComponent();
     }
 
-
     private void TicketList_Selected(object sender, RoutedEventArgs e)
     {
         try
         {
             var parent = this.Parent as FrameworkElement;
-            MainPageViewModel parentValue = new MainPageViewModel();
-            if (parent != null)
-            {
-                parentValue = parent.DataContext as MainPageViewModel;
-            }
+            var parentValue = parent?.DataContext as MainPageViewModel;
 
-
-            if (TicketList.SelectedItem != null)
+            if (TicketList.SelectedItem is TicketsListViewModel ticket && parentValue != null)
             {
-                var ticket = TicketList.SelectedItem as TicketsListViewModel;
-                var Details = new TicketDetailsWindow(ticket.Id, parentValue.CurrentFrame, null, null);
-                Details?.Show();
+                var details = new TicketDetailsWindow(ticket.Id, parentValue.CurrentFrame, null, null);
+                details?.Show();
                 TicketList.SelectedItem = null;
             }
-
         }
-        catch (Exception ex)
+        catch (Exception)
         {
             Application.Current.Dispatcher.Invoke(async () =>
             {
-                Wpf.Ui.Controls.MessageBox ms = new Wpf.Ui.Controls.MessageBox();
-                ms.FlowDirection = System.Windows.FlowDirection.RightToLeft;
-                ms.Title = "خطا";
-                ms.Content = "خطا در نمایش قبض، لطفا مجددا تلاش نمایید";
-                ms.IsPrimaryButtonEnabled = false;
-                ms.IsSecondaryButtonEnabled = false;
-                ms.CloseButtonText = "متوجه شدم";
+                var ms = new Wpf.Ui.Controls.MessageBox
+                {
+                    FlowDirection = System.Windows.FlowDirection.RightToLeft,
+                    Title = "خطا",
+                    Content = "خطا در نمایش قبض، لطفا مجددا تلاش نمایید",
+                    IsPrimaryButtonEnabled = false,
+                    IsSecondaryButtonEnabled = false,
+                    CloseButtonText = "متوجه شدم"
+                };
                 await ms.ShowDialogAsync();
             });
         }
-
     }
 }
