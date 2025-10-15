c8bcf1b660b078215b3ece29d73dc1d2e02373ec
Mehdi <mahdimrm01@gmail.com>
2025-10-15 17:10:01 +0330

    update : close the ticket detail window after succesful payment result

--- Parking.Domain/Entities/Parkings/ParkingSection.cs
+++ Parking.Domain/Entities/Parkings/ParkingSection.cs
@@ -1,11 +1,4 @@
-﻿using Parking.Domain.Entities.User;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Parking.Domain.Entities.Parkings;
 
