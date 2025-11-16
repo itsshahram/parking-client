fc5a1c63bf0df83fae40bebfb768f4cc9813be0e
Mehdi <mahdimrm01@gmail.com>
2025-11-16 19:38:52 +0330

    delete unused

--- Parking.App/Helpers/DatabaseHelperService.cs
+++ Parking.App/Helpers/DatabaseHelperService.cs
@@ -1,21 +1,11 @@
 ﻿namespace Parking.App.Helpers;
-
-using static Parking.App.Helpers.Constants;
-
 public class DatabaseMonitorService
 {
-    private readonly DbContextOptions<ApplicationDbContext> _options;
     private System.Threading.Timer? _timer;
     private bool _isReachable = true;
 
     public event Action? DatabaseLost;
     public event Action? DatabaseRestored;
-
-    public DatabaseMonitorService(DbContextOptions<ApplicationDbContext> options)
-    {
-        _options = options;
-    }
-
     public void StartMonitoring(int intervalSeconds = 5)
     {
         _timer = new System.Threading.Timer(async _ => await CheckDatabaseAsync(), null, 0, intervalSeconds * 1000);
